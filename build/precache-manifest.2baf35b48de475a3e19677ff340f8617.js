6ff3a95273ea8ec606382670fd3d0e45db2c1307
jaka suganda <jaksug@MacBook-Pro.local>
2020-02-08 15:45:14 +0700

    build

---
+++ build/precache-manifest.2baf35b48de475a3e19677ff340f8617.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13bad8e359927b2e590c1b90d2ac01f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f665ae333c99e6029fc5",
+    "url": "/static/css/2.1e105a3b.chunk.css"
+  },
+  {
+    "revision": "d2ac147d9bc9b3a4d7e7",
+    "url": "/static/css/main.3f9a9d5e.chunk.css"
+  },
+  {
+    "revision": "f665ae333c99e6029fc5",
+    "url": "/static/js/2.b73aa33c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.b73aa33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ac147d9bc9b3a4d7e7",
+    "url": "/static/js/main.cf7a09af.chunk.js"
+  },
+  {
+    "revision": "78d1c3a844fba4bd5f42",
+    "url": "/static/js/runtime-main.32d9ab9f.js"
+  }
+]);
